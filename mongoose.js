7b03ca02f97f34cd15bcbfb7771eaec0c7fb9593
shovancak <s.hovancak@gmail.com>
2020-06-29 18:57:46 +0200

    Using Mongoose for connecting to database and creating a product in database

--- mongoose.js
+++ mongoose.js
@@ -2,11 +2,25 @@ const mongoose = require("mongoose");
 
 const Product = require("./models/product");
 
+mongoose
+  .connect(
+    "mongodb+srv://samuel:83461834Sh@cluster0-eamri.mongodb.net/products_test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database.");
+  })
+  .catch(() => {
+    console.log("Connection failed,");
+  });
+
 const createProduct = async (req, res, next) => {
   const createdProduct = new Product({
     name: req.body.name,
     price: req.body.price,
   });
+  const result = await createdProduct.save();
+
+  res.json(result);
 };
 
 exports.createProduct = createProduct;
